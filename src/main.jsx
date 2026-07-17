a0be981c6de63154ce9761a1901c00218472c50b
gtmarketing <gtmarketing5300l@gmail.com>
2026-07-17 14:34:32 +0530

    Added Google Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-CKVWNWFK0C");
+ReactGA.send("pageview");
 
 import AOS from "aos";
 import "aos/dist/aos.css";
