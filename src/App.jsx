34412173a9b8ae4ac147a09e6342937e82be0880
gtmarketing <gtmarketing5300l@gmail.com>
2026-07-24 18:48:17 +0530

    Rename Layout.js to Layout.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
-import Layout from "./layouts/layout";
-
+import Layout from "./layouts/Layout";
 import Home from "./pages/Home";
 import SeoTips from "./pages/SeoTips";
 import GoogleAdsVsFacebookAds from "./pages/GoogleAdsVsFacebookAds";
