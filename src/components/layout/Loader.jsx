94ad8d651f3a8c90168481a1722356c43ef99a07
gtmarketing <gtmarketing5300l@gmail.com>
2026-07-27 18:39:26 +0530

    Updated multimedia website

--- src/components/layout/Loader.jsx
+++ src/components/layout/Loader.jsx
@@ -6,7 +6,7 @@ export default function Loader() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 500);
+    }, 900);
 
     return () => clearTimeout(timer);
   }, []);
