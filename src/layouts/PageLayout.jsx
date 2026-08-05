6007e24302804252dafe4665f30b418e81829ccd
gtmarketing <gtmarketing5300l@gmail.com>
2026-08-05 10:57:33 +0530

    Updated multimedia website

--- src/layouts/PageLayout.jsx
+++ src/layouts/PageLayout.jsx
@@ -1,20 +1,24 @@
-import { Outlet } from "react-router-dom";
-import Loader from "../components/layout/Loader";
-import Navbar from "../components/layout/Navbar";
-import Footer from "../components/layout/Footer";
-import BackToTop from "../components/layout/BackToTop";
-import FloatingButtons from "../components/layout/FloatingButtons";
+import { useEffect, useState } from "react";
 
-export default function Layout() {
+export default function Loader() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    console.log("Loader Mounted");
+
+    const timer = setTimeout(() => {
+      console.log("Loader Hidden");
+      setLoading(false);
+    }, 5000); // 5 seconds માટે test
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!loading) return null;
+
   return (
-    <>
-      <Loader />
-
-      <Navbar />
-      <Outlet />
-      <Footer />
-      <BackToTop />
-      <FloatingButtons />
-    </>
+    <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-blue-700">
+      <h1 className="text-5xl font-bold text-white">LOADER WORKING...</h1>
+    </div>
   );
 }
